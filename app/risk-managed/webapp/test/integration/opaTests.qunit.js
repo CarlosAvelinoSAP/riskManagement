76cb8c73482b6f939ff1af313bb533f1ad976199
CARLOS Jose Avelino <carlos.avelino.prestserv@petrobras.com.br>
2024-10-17 17:43:47 +0000

    Create Risk-Managed App

---
+++ app/risk-managed/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,26 @@
+sap.ui.require(
+    [
+        'sap/fe/test/JourneyRunner',
+        'riskmanaged/test/integration/FirstJourney',
+		'riskmanaged/test/integration/pages/RisksList',
+		'riskmanaged/test/integration/pages/RisksObjectPage'
+    ],
+    function(JourneyRunner, opaJourney, RisksList, RisksObjectPage) {
+        'use strict';
+        var JourneyRunner = new JourneyRunner({
+            // start index.html in web folder
+            launchUrl: sap.ui.require.toUrl('riskmanaged') + '/index.html'
+        });
+
+       
+        JourneyRunner.run(
+            {
+                pages: { 
+					onTheRisksList: RisksList,
+					onTheRisksObjectPage: RisksObjectPage
+                }
+            },
+            opaJourney.run
+        );
+    }
+);
